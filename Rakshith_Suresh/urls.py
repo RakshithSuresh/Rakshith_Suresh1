654b883495ba092ff3c81ab165c9f298171114bd
Rakshith Suresh <rakshithsuresh21@gmail.com>
2023-12-12 01:13:15 -0800

    Initial Commit

---
+++ Rakshith_Suresh/urls.py
@@ -0,0 +1,9 @@
+# contact_manager/urls.py
+
+from django.contrib import admin
+from django.urls import path, include
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('', include('myapp.urls')),
+]
